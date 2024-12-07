943b34706ff59294ccdbcaa5e75f8d8578aedb54
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2024-12-07 12:46:31 +0530

    feat: alternative wallet actions

--- src/utils/contract/actions.ts
+++ src/utils/contract/actions.ts
@@ -1,6 +1,11 @@
-import { getUserItemsContract } from "./contract";
 import { ChainId } from "../../config";
 import { Address } from "viem";
+import { getWalletClient } from "./viemClient";
+import { abi } from "../../abis/UserItems.json";
+
+export const addressByChainId = {
+  137: "0x3365c64eB8416a8168d95b4c265651a6ebC51Cd1",
+} as const;
 
 export const mintTokens = async (
   chainId: ChainId,
@@ -8,9 +13,14 @@ export const mintTokens = async (
   tokenId: number,
   amount: number
 ) => {
-  // const contract = getUserItemsContract(chainId);
-  // const hash = await contract.write.mint([account, tokenId, amount, ""]);
-  return "hash";
+  const walletClient = getWalletClient(chainId);
+  const hash = await walletClient.writeContract({
+    abi,
+    address: addressByChainId[chainId],
+    functionName: "mint",
+    args: [account, tokenId, amount, ""],
+  });
+  return hash;
 };
 
 export const burnTokens = async (
@@ -19,7 +29,12 @@ export const burnTokens = async (
   tokenId: number,
   amount: number
 ) => {
-  // const contract = getUserItemsContract(chainId);
-  // const hash = await contract.write.burn([account, tokenId, amount]);
-  return "hash";
+  const walletClient = getWalletClient(chainId);
+  const hash = await walletClient.writeContract({
+    abi,
+    address: addressByChainId[chainId],
+    functionName: "burn",
+    args: [account, tokenId, amount],
+  });
+  return hash;
 };
