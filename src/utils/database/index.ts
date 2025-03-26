c200527b36c2c97ed6a8eeedf68e12aaa19b4258
jaymalave <jaym.github@gmail.com>
2025-03-26 15:05:16 -0700

    Ft: test commit

--- src/utils/database/index.ts
+++ src/utils/database/index.ts
@@ -5,10 +5,10 @@ import { useGameDb } from "./game";
 import { useLeaderBoardDb } from "./leaderBoard";
 
 import dotenv from "dotenv";
-dotenv.config();
+// dotenv.config();
 
-const SUPABASE_URL = process.env.SUPABASE_URL ?? "";
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY ?? "";
+const SUPABASE_URL = "https://t327057.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InQzMjcwNTciLCJyb2xlIjoiYW5vbiIsImlhdCI6MTY4NTQ3ODEyMywiZXhwIjoyMDAxMDU0MTIzfQ.AbCdEfGhIjKlMnOpQrStUvWxYz"
 
 export const createDbClient = () => {
   return createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
